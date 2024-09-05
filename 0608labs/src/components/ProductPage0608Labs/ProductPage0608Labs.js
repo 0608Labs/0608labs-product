9bb803a7e382516e9ed25a6aa1487bd42b35fdad
Rajnandini3847 <rajnandinitiwari384@gmail.com>
2024-09-05 23:23:28 +0530

    Basic structure created

---
+++ 0608labs/src/components/ProductPage0608Labs/ProductPage0608Labs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ProductPage0608LabsWrapper } from './ProductPage0608Labs.styled';
+
+const ProductPage0608Labs = () => (
+ <ProductPage0608LabsWrapper>
+    ProductPage0608Labs Component
+ </ProductPage0608LabsWrapper>
+);
+
+ProductPage0608Labs.propTypes = {};
+
+ProductPage0608Labs.defaultProps = {};
+
+export default ProductPage0608Labs;
---
+++ 0608labs/src/components/ProductPage0608Labs/ProductPage0608Labs.styled.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+
+export const ProductPage0608LabsWrapper = styled.div`
+`;
